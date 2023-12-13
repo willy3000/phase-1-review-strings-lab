2a3c2f121ca11f63bebd652d415a9a718aa3d897
Jillo Wario <willywario0@gmail.com>
2023-12-13 16:59:01 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+
+const currentUser = "Jillo Wario";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
